6b54645656019ed27c375cf2e5d1a518d8bb6720
vincent-chege <121945862+vincent-chege@users.noreply.github.com>
2023-08-05 18:44:59 +0300

    Add files via upload

---
+++ pipewire-installer.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Add Pipewire repository
+sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
+
+# Update package lists
+sudo apt update
+
+# Install required packages
+sudo apt install -y pipewire libspa-0.2-bluetooth pipewire-audio-client-libraries wireplumber pipewire-pulse
+
+# Remove PulseAudio (optional)
+sudo apt remove -y pulseaudio
+
+# Disable and mask PulseAudio services (optional)
+systemctl --user --now disable pulseaudio.service pulseaudio.socket
+systemctl --user mask pulseaudio
+
+# Enable and start Pipewire and WirePlumber services
+systemctl --user --now enable pipewire pipewire-pulse wireplumber
+
+echo "Installation and configuration completed."
