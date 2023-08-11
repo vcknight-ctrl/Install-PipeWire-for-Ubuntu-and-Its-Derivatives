8e7df6e1bc9dda6e8862f4d46a2dc141d5db3bc7
vincent-chege <121945862+vincent-chege@users.noreply.github.com>
2023-08-11 12:06:19 +0300

    Update pipewire-installer.sh

--- pipewire-installer.sh
+++ pipewire-installer.sh
@@ -7,10 +7,13 @@ sudo add-apt-repository ppa:pipewire-debian/pipewire-upstream
 sudo apt update
 
 # Install required packages
-sudo apt install -y pipewire libspa-0.2-bluetooth pipewire-audio-client-libraries wireplumber pipewire-pulse
+sudo apt install -y pipewire libldacbt-{abr,enc}2 libspa-0.2-bluetooth pipewire-audio-client-libraries wireplumber pipewire-pulse
 
 # Remove PulseAudio (optional)
-sudo apt remove -y pulseaudio
+#sudo apt remove -y pulseaudio
+
+# Config Files
+sudo cp /usr/share/doc/pipewire/examples/alsa.conf.d/99-pipewire-default.conf /etc/alsa/conf.d/
 
 # Disable and mask PulseAudio services
 systemctl --user --now disable pulseaudio.service pulseaudio.socket
@@ -18,5 +21,6 @@ systemctl --user mask pulseaudio
 
 # Enable and start Pipewire and WirePlumber services
 systemctl --user --now enable pipewire pipewire-pulse wireplumber
+systemctl --user --now enable wireplumber.service
 
 echo "Installation and configuration completed."
